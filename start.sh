7a5ae4ba12b3525a3fc84bde92c27f0af5c56bd3
wangtao <l>
2024-01-05 14:58:14 +0800

    feat: new1

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
  # @Author: Wangtao
  # @Date: 2024-01-05 11:38:13
  # @LastEditors: Wangtao
- # @LastEditTime: 2024-01-05 11:40:19
+ # @LastEditTime: 2024-01-05 14:58:00
 ### 
 echo '进来了'
 python3 run.py 0.0.0.0 8080 &
 cd /wxcloudrun-wxcomponent
 ./main
 
-cd /node
-pm2 start index.js
+# cd /node
+# pm2 start index.js
 
